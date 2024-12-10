a10b65d3b8393559168d02caa995158333e37d4b
j1nwan <weamicron@gmail.com>
2024-12-10 21:54:40 +0900

    More 버튼 이벤트 추가

--- lib/look/widget/search_grid_widget.dart
+++ lib/look/widget/search_grid_widget.dart
@@ -1,7 +1,7 @@
 import 'dart:math';
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:scribble_todo/common/theme/common_colors.dart';
 
 class SearchGridWidget extends StatelessWidget {
   final String userName;
@@ -39,55 +39,83 @@ class SearchGridWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
-      child: Column(
-        children: [
-          Row(
-            children: [
-              Container(
-                width: 45,
-                height: 45,
-                decoration: BoxDecoration(
-                  color: randomColor(),
-                  borderRadius: BorderRadius.circular(30),
+    return GestureDetector(
+      onTap: () {
+        //Navigator.push(context, MaterialPageRoute(builder: (context) => const FeedScreen()));
+        print('다른 사용자 Feed 화면으로 이동');
+      },
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Container(
+                  width: 45,
+                  height: 45,
+                  decoration: BoxDecoration(
+                    color: randomColor(),
+                    borderRadius: BorderRadius.circular(30),
+                  ),
                 ),
-              ),
-              const SizedBox(width: 10),
-              Expanded(
-                child: Text(
-                  userName,
-                  style: const TextStyle(fontSize: 13, fontWeight: FontWeight.bold),
+                const SizedBox(width: 10),
+                Expanded(
+                  child: Text(
+                    userName,
+                    style: const TextStyle(fontSize: 13, fontWeight: FontWeight.bold),
+                  ),
                 ),
-              ),
-              IconButton(
-                onPressed: () {},
-                icon: const Icon(Icons.more_horiz),
-              )
-            ],
-          ),
-          Row(
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(left: 55),
-                child: Column(
-                  children: [
-                    SizedBox(
-                      width: MediaQuery.of(context).size.width * 0.7,
-                      height: 100,
-                      child: division == 'todo'
-                          ? Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: randomTodoWidget(),
-                            )
-                          : const Text('오늘 할일 없음\n그래서 매우 슬프다.'),
-                    ),
-                  ],
-                ),
-              )
-            ],
-          )
-        ],
+                IconButton(
+                  onPressed: () {
+                    showCupertinoModalPopup(
+                      context: context,
+                      builder: (context) => CupertinoActionSheet(
+                        actions: [
+                          CupertinoActionSheetAction(
+                            onPressed: () {
+                              Navigator.pop(context);
+                              print('신고하기');
+                            },
+                            child: const Text('신고하기'),
+                          ),
+                        ],
+                        cancelButton: CupertinoActionSheetAction(
+                          onPressed: () {
+                            Navigator.pop(context);
+                            print('취소');
+                          },
+                          child: const Text('취소'),
+                        ),
+                      ),
+                    );
+                  },
+                  icon: const Icon(Icons.more_horiz),
+                )
+              ],
+            ),
+            Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 55),
+                  child: Column(
+                    children: [
+                      SizedBox(
+                        width: MediaQuery.of(context).size.width * 0.7,
+                        height: 100,
+                        child: division == 'todo'
+                            ? Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: randomTodoWidget(),
+                              )
+                            : const Text('오늘 할일 없음\n그래서 매우 슬프다.'),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
