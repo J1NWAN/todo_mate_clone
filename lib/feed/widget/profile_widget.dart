f01309cfcbedf422a2fd3abb0f08c0a8b98ff72d
j1nwan <weamicron@gmail.com>
2024-12-10 21:55:17 +0900

    일기 화면 이동 기능 추가

--- lib/feed/widget/profile_widget.dart
+++ lib/feed/widget/profile_widget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:scribble_todo/common/theme/common_colors.dart';
+import 'package:scribble_todo/feed/screen/diary_screen.dart';
 
 class ProfileWidget extends StatelessWidget {
   const ProfileWidget({super.key});
@@ -30,7 +31,15 @@ class ProfileWidget extends StatelessWidget {
             ],
           ),
           IconButton(
-            onPressed: () {},
+            onPressed: () {
+              Navigator.push(
+                context,
+                CupertinoPageRoute(
+                  builder: (context) => const DiaryScreen(),
+                  fullscreenDialog: true,
+                ),
+              );
+            },
             icon: const Icon(
               CupertinoIcons.pencil_outline,
               color: Color.fromRGBO(140, 140, 200, 1.0),
