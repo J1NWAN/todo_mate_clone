c8b6e5efcf649cacb244c6402ab0fb7cfbcbe3d8
j1nwan <weamicron@gmail.com>
2024-12-11 21:48:24 +0900

    바텀 모달 위젯 선택 기능 추가 및 일기 작성 필드 추가

--- lib/feed/screen/diary_screen.dart
+++ lib/feed/screen/diary_screen.dart
@@ -5,177 +5,208 @@ import 'package:flutter/widgets.dart';
 import 'package:intl/intl.dart';
 import 'package:scribble_todo/common/theme/common_colors.dart';
 
-class DiaryScreen extends StatelessWidget {
+class DiaryScreen extends StatefulWidget {
   const DiaryScreen({super.key});
 
   @override
+  State<DiaryScreen> createState() => _DiaryScreenState();
+}
+
+class _DiaryScreenState extends State<DiaryScreen> {
+  String selectedValue = '나만보기';
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('일기', style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold)),
-        actions: [
-          TextButton(
-            onPressed: () {},
-            child: const Text('완료', style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold, color: Colors.white)),
-          )
-        ],
-      ),
-      body: Column(
-        children: [
-          Align(
-            alignment: Alignment.center,
-            child: IconButton(
+    return GestureDetector(
+      onTap: () {
+        FocusScopeNode currentFocus = FocusScope.of(context);
+        if (!currentFocus.hasPrimaryFocus) {
+          currentFocus.unfocus();
+        }
+      },
+      child: Scaffold(
+        resizeToAvoidBottomInset: false,
+        appBar: AppBar(
+          title: const Text('일기', style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold)),
+          actions: [
+            TextButton(
               onPressed: () {},
-              icon: const Icon(
-                CupertinoIcons.smiley,
-                size: 50,
-                color: Color.fromRGBO(140, 140, 200, 1.0),
-              ),
-            ),
-          ),
-          const SizedBox(height: 20),
-          Row(
-            children: [
-              Container(
-                margin: const EdgeInsets.only(right: 10),
-                width: 35,
-                height: 35,
-                decoration: BoxDecoration(
-                  color: CommonColors.primaryColor,
-                  borderRadius: BorderRadius.circular(50),
+              child: const Text('완료', style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold, color: Colors.white)),
+            )
+          ],
+        ),
+        body: Column(
+          children: [
+            Align(
+              alignment: Alignment.center,
+              child: IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  CupertinoIcons.smiley,
+                  size: 50,
+                  color: Color.fromRGBO(140, 140, 200, 1.0),
                 ),
               ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const Text(
-                    'me',
-                    style: TextStyle(fontSize: 12, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 20),
+            Row(
+              children: [
+                Container(
+                  margin: const EdgeInsets.only(right: 10),
+                  width: 35,
+                  height: 35,
+                  decoration: BoxDecoration(
+                    color: CommonColors.primaryColor,
+                    borderRadius: BorderRadius.circular(50),
                   ),
-                  Row(
-                    children: [
-                      Text(
-                        DateFormat('yyyy년 MM월 dd일 EEEE', 'ko').format(DateTime.now()),
-                        style: const TextStyle(fontSize: 12),
-                      ),
-                      const SizedBox(width: 10),
-                      GestureDetector(
-                        onTap: () {
-                          showModalBottomSheet(
-                            context: context,
-                            builder: (context) {
-                              int selectedValue = 0;
-                              return StatefulBuilder(builder: (context, setState) {
-                                return Container(
-                                  height: 380,
-                                  padding: const EdgeInsets.all(20),
-                                  child: Column(
-                                    crossAxisAlignment: CrossAxisAlignment.start,
-                                    children: [
-                                      const Align(
-                                        alignment: Alignment.center,
-                                        child: Text(
-                                          '공개 설정',
-                                          style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
-                                        ),
-                                      ),
-                                      const SizedBox(height: 20),
-                                      ListTile(
-                                        leading: const Icon(Icons.public),
-                                        title: const Text('전체 공개'),
-                                        trailing: Radio(
-                                          value: 0,
-                                          groupValue: selectedValue,
-                                          onChanged: (value) {
-                                            setState(() {
-                                              selectedValue = value!;
-                                            });
-                                          },
-                                        ),
-                                      ),
-                                      const Divider(height: 0),
-                                      ListTile(
-                                        leading: const Icon(Icons.group),
-                                        title: const Text('팔로워 공개'),
-                                        trailing: Radio(
-                                          value: 1,
-                                          groupValue: selectedValue,
-                                          onChanged: (value) {
-                                            setState(() {
-                                              selectedValue = value!;
-                                            });
-                                          },
-                                        ),
-                                      ),
-                                      const Divider(height: 0),
-                                      ListTile(
-                                        leading: const Icon(Icons.group_outlined),
-                                        title: const Text('일부 공개'),
-                                        trailing: Radio(
-                                          value: 2,
-                                          groupValue: selectedValue,
-                                          onChanged: (value) {
-                                            setState(() {
-                                              selectedValue = value!;
-                                            });
-                                          },
-                                        ),
-                                      ),
-                                      const Divider(height: 0),
-                                      ListTile(
-                                        leading: const Icon(Icons.lock),
-                                        title: const Text('나만보기'),
-                                        trailing: Radio(
-                                          value: 3,
-                                          groupValue: selectedValue,
-                                          onChanged: (value) {
-                                            setState(() {
-                                              selectedValue = value!;
-                                            });
-                                          },
-                                        ),
-                                      ),
-                                      const Divider(height: 0),
-                                      const SizedBox(height: 10),
-                                      SizedBox(
-                                        width: double.infinity,
-                                        child: ElevatedButton(
-                                          onPressed: () {
-                                            Navigator.pop(context);
-                                          },
-                                          style: ElevatedButton.styleFrom(
-                                            backgroundColor: CommonColors.secondaryColor2,
-                                            shape: RoundedRectangleBorder(
-                                              borderRadius: BorderRadius.circular(10),
-                                            ),
-                                          ),
-                                          child: const Text('확인', style: TextStyle(fontSize: 14, color: Colors.white)),
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                );
-                              });
-                            },
-                          );
-                        },
-                        child: const Row(
-                          children: [
-                            Icon(Icons.lock, size: 16, color: Colors.grey),
-                            SizedBox(width: 5),
-                            Text('나만보기', style: TextStyle(fontSize: 12, color: Colors.white)),
-                            Icon(Icons.arrow_drop_down, size: 16, color: Colors.white),
-                          ],
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      'me',
+                      style: TextStyle(fontSize: 12, fontWeight: FontWeight.bold),
+                    ),
+                    Row(
+                      children: [
+                        Text(
+                          DateFormat('yyyy년 MM월 dd일 EEEE', 'ko').format(DateTime.now()),
+                          style: const TextStyle(fontSize: 12),
                         ),
-                      )
-                    ],
+                        const SizedBox(width: 10),
+
+                        // 바텀 모달 위젯
+                        showModalBottomWidget(),
+                      ],
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.all(4.0),
+              child: TextField(
+                maxLines: 10,
+                decoration: InputDecoration(
+                  hintText: 'me님의 오늘은 어떤 하루였나요?',
+                  hintStyle: TextStyle(fontSize: 12, color: CommonColors.secondaryColor2),
+                  enabledBorder: const OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.black),
                   ),
-                ],
-              )
-            ],
-          ),
+                  focusedBorder: const OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.black),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  // 바텀 모달 위젯
+  Widget showModalBottomWidget() {
+    return GestureDetector(
+      onTap: () {
+        showModalBottomSheet(
+          context: context,
+          builder: (context) {
+            return StatefulBuilder(builder: (context, setModalState) {
+              return Container(
+                height: 420,
+                padding: const EdgeInsets.all(20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Align(
+                      alignment: Alignment.center,
+                      child: Text(
+                        '공개 설정',
+                        style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    SizedBox(
+                      height: 250,
+                      child: ListView.builder(
+                        shrinkWrap: true,
+                        itemCount: 4,
+                        itemBuilder: (context, index) {
+                          return showModalBottomItemWidget(index, (String value) {
+                            setModalState(() {
+                              selectedValue = value;
+                            });
+                          });
+                        },
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    SizedBox(
+                      width: double.infinity,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          setState(() {
+                            selectedValue = selectedValue;
+                          });
+                          Navigator.pop(context);
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: CommonColors.secondaryColor2,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                        child: const Text('확인', style: TextStyle(fontSize: 14, color: Colors.white)),
+                      ),
+                    ),
+                  ],
+                ),
+              );
+            });
+          },
+        );
+      },
+      child: Row(
+        children: [
+          Icon(
+              selectedValue == '나만보기'
+                  ? Icons.lock
+                  : selectedValue == '전체 공개'
+                      ? Icons.public
+                      : selectedValue == '팔로워 공개'
+                          ? Icons.group
+                          : Icons.group_outlined,
+              size: 16,
+              color: Colors.grey),
+          const SizedBox(width: 5),
+          Text(selectedValue, style: const TextStyle(fontSize: 12, color: Colors.white)),
+          const Icon(Icons.arrow_drop_down, size: 16, color: Colors.white),
         ],
       ),
+    );
+  }
+
+  // 바텀 모달 아이템 위젯
+  Widget showModalBottomItemWidget(int index, Function(String) setModalState) {
+    List<String> list = ['전체 공개', '팔로워 공개', '일부 공개', '나만보기'];
+    List<IconData> iconList = [Icons.public, Icons.group, Icons.group_outlined, Icons.lock];
+
+    return Column(
+      children: [
+        ListTile(
+          leading: Icon(iconList[index]),
+          title: Text(list[index]),
+          trailing: Radio(
+            value: list[index],
+            groupValue: selectedValue,
+            onChanged: (value) {
+              setModalState(value!);
+            },
+          ),
+        ),
+        const Divider(height: 0),
+      ],
     );
   }
 }
